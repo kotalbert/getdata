e5ec043f036cb5dd377ba7fb414bf946a74868dd
Pawel Daniluk <pawel@daniluk.waw.pl>
2016-10-10 22:09:44 +0200

    added comments

--- project/run_analysis.R
+++ project/run_analysis.R
@@ -1,9 +1,12 @@
-
+# This program was created for the Getting and Cleaning Data course project
+# The program:
+# Downloads the files from the Internet to be used in further operations
 # Merges the training and the test sets to create one data set.
 # Extracts only the measurements on the mean and standard deviation for each measurement.
 # Uses descriptive activity names to name the activities in the data set
 # Appropriately labels the data set with descriptive variable names.
 # From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 
 # Download and unzip the files into /data catalogue.
 # This part of program can be used to perform analysis from scratch
